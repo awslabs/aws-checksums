a981dffa5ffabe8a7c3a0e2c20759f5ba02c7fdd
Jonathan M. Henson <henso@amazon.com>
2018-11-19 11:52:31 -0800

    Added gcc-8 builds, made sure release build path is taken for gcc.

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,9 +1,9 @@
 
 mkdir build
 cd build
-cmake %* -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
-msbuild.exe aws-checksums.vcxproj /p:Configuration=Release || goto error
-msbuild.exe aws-checksums-tests.vcxproj /p:Configuration=Release || goto error
+cmake %* -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+msbuild.exe aws-checksums.vcxproj /p:Configuration=RelWithDebInfo || goto error
+msbuild.exe aws-checksums-tests.vcxproj /p:Configuration=RelWithDebInfo || goto error
 ctest -V
 
 goto :EOF
