1fea5fd98adef3e513d1bd9ebded80fc7dc31c88
Jonathan M. Henson <henso@amazon.com>
2020-06-02 12:33:44 -0700

    Clang is a bloody lier. Also disabled the asm on old assemblers. (#24)
    * Clang is a bloody lier. Also disabled the asm on old assemblers.

--- source/intel/crc32c_sse42_asm.c
+++ source/intel/crc32c_sse42_asm.c
@@ -18,8 +18,8 @@
 
 /*this implementation is only for 64 bit arch and (if on GCC, release mode).
  * If using clang, this will run for both debug and release.*/
-#if defined(__x86_64__) && !(defined(__GNUC__) && defined(DEBUG_BUILD))
-
+#if defined(__x86_64__) &&                                                                                             \
+    (defined(__clang__) || !((defined(__GNUC__)) && ((__GNUC__ == 4 && __GNUC_MINOR__ < 4) || defined(DEBUG_BUILD))))
 #    define LIKELY(x) __builtin_expect((x), 1)
 #    define UNLIKELY(x) __builtin_expect((x), 0)
 
@@ -113,7 +113,7 @@ static inline uint32_t s_crc32c_sse42_clmul_256(const uint8_t *input, uint32_t c
                  : "+c"(crc)
 
                  /* input registers */
-                 : [crc] "c"(crc), [in] "d"(input)
+                 : [ crc ] "c"(crc), [ in ] "d"(input)
 
                  /* additional clobbered registers */
                  : "%r8", "%r9", "%r11", "%r10", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "cc");
@@ -203,7 +203,7 @@ static inline uint32_t s_crc32c_sse42_clmul_1024(const uint8_t *input, uint32_t 
 
         /* input registers */
         /* the numeric values match the position of the output registers */
-        : [crc] "c"(crc), [in] "d"(input)
+        : [ crc ] "c"(crc), [ in ] "d"(input)
 
         /* additional clobbered registers */
         /* "cc" is the flags - we add and sub, so the flags are also clobbered */
@@ -283,7 +283,7 @@ static inline uint32_t s_crc32c_sse42_clmul_3072(const uint8_t *input, uint32_t 
 
         /* input registers
            the numeric values match the position of the output registers */
-        : [crc] "c"(crc), [in] "d"(input)
+        : [ crc ] "c"(crc), [ in ] "d"(input)
 
         /* additional clobbered registers
           "cc" is the flags - we add and sub, so the flags are also clobbered */
@@ -317,7 +317,7 @@ uint32_t aws_checksums_crc32c_hw(const uint8_t *input, int length, uint32_t prev
     /* For small input, forget about alignment checks - simply compute the CRC32c one byte at a time */
     if (UNLIKELY(length < 8)) {
         while (length-- > 0) {
-            asm("loop_small_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [crc] "c"(crc), [in] "r"(input));
+            asm("loop_small_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [ crc ] "c"(crc), [ in ] "r"(input));
             input++;
         }
         return ~crc;
@@ -334,7 +334,7 @@ uint32_t aws_checksums_crc32c_hw(const uint8_t *input, int length, uint32_t prev
 
     /* spin through the leading unaligned input bytes (if any) one-by-one */
     while (leading-- > 0) {
-        asm("loop_leading_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [crc] "c"(crc), [in] "r"(input));
+        asm("loop_leading_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [ crc ] "c"(crc), [ in ] "r"(input));
         input++;
     }
 
@@ -364,14 +364,14 @@ uint32_t aws_checksums_crc32c_hw(const uint8_t *input, int length, uint32_t prev
     /* Spin through remaining (aligned) 8-byte chunks using the CRC32Q quad word instruction */
     while (LIKELY(length >= 8)) {
         /* Hardcoding %rcx register (i.e. "+c") to allow use of qword instruction */
-        asm volatile("loop_8_%=: CRC32Q (%[in]), %%rcx" : "+c"(crc) : [crc] "c"(crc), [in] "r"(input));
+        asm volatile("loop_8_%=: CRC32Q (%[in]), %%rcx" : "+c"(crc) : [ crc ] "c"(crc), [ in ] "r"(input));
         input += 8;
         length -= 8;
     }
 
     /* Finish up with any trailing bytes using the CRC32B single byte instruction one-by-one */
     while (length-- > 0) {
-        asm volatile("loop_trailing_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [crc] "c"(crc), [in] "r"(input));
+        asm volatile("loop_trailing_%=: CRC32B (%[in]), %[crc]" : "+c"(crc) : [ crc ] "c"(crc), [ in ] "r"(input));
         input++;
     }
 
