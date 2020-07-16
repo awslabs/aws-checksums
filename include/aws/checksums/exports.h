0387547f89e44d3d90a930a0de2d6e6a7ea8a917
Andrew Tang <tangandr@amazon.com>
2020-07-16 12:38:20 -0700

    fix

--- include/aws/checksums/exports.h
+++ include/aws/checksums/exports.h
@@ -4,7 +4,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-#if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(_WIN32)
+#if defined(AWS_C_RT_USE_WINDOWS_DLL_SEMANTICS) || defined(_WIN32)
 #    ifdef USE_IMPORT_EXPORT
 #        ifdef AWS_CHECKSUMS_EXPORTS
 #            define AWS_CHECKSUMS_API __declspec(dllexport)
@@ -14,12 +14,13 @@
 #    else
 #        define AWS_CHECKSUMS_API
 #    endif /* USE_IMPORT_EXPORT */
-#else      /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32) */
-#    if ((__GNUC__ >= 4) || defined(__clang__))
+#else      /* defined (AWS_C_RT_USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32) */
+#    if ((__GNUC__ >= 4) || defined(__clang__)) && defined(AWS_CHECKSUMS_USE_IMPORT_EXPORT) &&                         \
+        defined(AWS_CHECKSUMS_EXPORTS)
 #        define AWS_CHECKSUMS_API __attribute__((visibility("default")))
 #    else
 #        define AWS_CHECKSUMS_API
 #    endif /* __GNUC__ >= 4 || defined(__clang__) */
-#endif     /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32) */
+#endif     /* defined (AWS_C_RT_USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32) */
 
 #endif /* AWS_CHECKSUMS_EXPORTS_H */
