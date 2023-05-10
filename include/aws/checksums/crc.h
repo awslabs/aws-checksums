a4ae535d863437c335456a67f45d69bb219bcdb2
Waqar Ahmed Khan <waahm7@gmail.com>
2023-05-10 08:49:07 -0700

    Add macro in public headers

--- include/aws/checksums/crc.h
+++ include/aws/checksums/crc.h
@@ -10,6 +10,8 @@
 
 AWS_PUSH_SANE_WARNING_LEVEL
 
+AWS_PUSH_SANE_WARNING_LEVEL
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -33,6 +35,7 @@ AWS_CHECKSUMS_API uint32_t aws_checksums_crc32c(const uint8_t *input, int length
 #ifdef __cplusplus
 }
 #endif
+AWS_POP_SANE_WARNING_LEVEL
 AWS_POP_SANE_WARNING_LEVEL
 
 #endif /* AWS_CHECKSUMS_CRC_H */
