598cc8d1774015e92c69d3ea230f3f758f1881ef
Waqar Ahmed Khan <waahm7@gmail.com>
2023-05-10 08:51:03 -0700

    fix extern c marco

--- include/aws/checksums/crc.h
+++ include/aws/checksums/crc.h
@@ -6,13 +6,11 @@
  */
 
 #include <aws/checksums/exports.h>
+#include <aws/common/macros.h>
 #include <stdint.h>
 
 AWS_PUSH_SANE_WARNING_LEVEL
-
-#ifdef __cplusplus
-extern "C" {
-#endif
+AWS_EXTERN_C_BEGIN
 
 /**
  * The entry point function to perform a CRC32 (Ethernet, gzip) computation.
@@ -30,10 +28,7 @@ AWS_CHECKSUMS_API uint32_t aws_checksums_crc32(const uint8_t *input, int length,
  */
 AWS_CHECKSUMS_API uint32_t aws_checksums_crc32c(const uint8_t *input, int length, uint32_t previousCrc32);
 
-#ifdef __cplusplus
-}
-#endif
-
+AWS_EXTERN_C_BEGIN
 AWS_POP_SANE_WARNING_LEVEL
 
 #endif /* AWS_CHECKSUMS_CRC_H */
