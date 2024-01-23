a3ab1932c5620edd14bf987f1a92a902a9c56513
Jonathan M. Henson <henso@amazon.com>
2024-01-23 12:57:26 -0800

    Maybe the headers i need for older compilers.

--- source/intel/intrin/crc64xz_clmul.c
+++ source/intel/intrin/crc64xz_clmul.c
@@ -10,6 +10,8 @@
 #    ifndef _MSC_VER
 #        include <x86intrin.h>
 #    else
+#        include <xmmintrin.h>
+#        include <mmintrin.h>
 #        include <immintrin.h>
 #    endif
 #    define load_xmm(ptr) _mm_loadu_si128((const __m128i *)(const void *)(ptr))
