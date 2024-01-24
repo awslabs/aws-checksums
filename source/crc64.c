449c8b4f41a026718e637c961a4c2399dcc597ec
Jonathan M. Henson <henso@amazon.com>
2024-01-24 13:57:18 -0800

    Use runtime cpu checks for arm.

--- source/crc64.c
+++ source/crc64.c
@@ -96,12 +96,11 @@ uint64_t aws_checksums_crc64xz(const uint8_t *input, int length, uint64_t previo
             s_crc64xz_fn_ptr = aws_checksums_crc64xz_sw;
         }
 #elif INTPTR_MAX == INT64_MAX && defined(AWS_HAVE_ARMv8_1)
-        // TODO need to add these feature flags to aws-c-common
-        // if (aws_cpu_has_feature(AWS_CPU_FEATURE_ARM_CRYPTO) && aws_cpu_has_feature(AWS_CPU_FEATURE_ARM_PMULL64)) {
-        s_crc64xz_fn_ptr = aws_checksums_crc64xz_arm_pmull;
-        //} else {
-        //    s_crc64xz_fn_ptr = aws_checksums_crc64xz_sw;
-        //}
+        if (aws_cpu_has_feature(AWS_CPU_FEATURE_ARM_CRYPTO) && aws_cpu_has_feature(AWS_CPU_FEATURE_ARM_PMULL)) {
+            s_crc64xz_fn_ptr = aws_checksums_crc64xz_arm_pmull;
+        } else {
+            s_crc64xz_fn_ptr = aws_checksums_crc64xz_sw;
+        }
 #else // defined(AWS_HAVE_ARMv8_1)
         s_crc64xz_fn_ptr = aws_checksums_crc64xz_sw;
 #endif
