35e05f9fba24dcec6a27a65f4918459688f699fa
DmitriyMusatkin <musatkd@amazon.com>
2024-09-12 13:34:54 -0700

    skip on openbsd

--- tests/crc64_test.c
+++ tests/crc64_test.c
@@ -129,7 +129,7 @@ AWS_TEST_CASE(test_crc64nvme, s_test_crc64nvme)
 
 static int s_test_large_buffer_crc64(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
-#if SIZE_BITS == 32
+#if SIZE_BITS == 32 || defined(__OpenBSD__) /* openbsd fails to allocate big buffer */
     (void)allocator;
     return AWS_OP_SKIP;
 #else
