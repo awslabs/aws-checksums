1dd1eb5b363a53d5176aa93e6139e7c21f41843b
DmitriyMusatkin <musatkd@amazon.com>
2024-10-29 01:47:59 -0700

    lint

--- include/aws/checksums/private/crc_util.h
+++ include/aws/checksums/private/crc_util.h
@@ -33,8 +33,8 @@ static inline uint32_t aws_bswap32_if_be(uint32_t x) {
 #elif defined(__GNUC__) || defined(__clang__)
     return __builtin_bswap32(x);
 #else
-    return (((x & 0xff000000u) >> 24) | ((x & 0x00ff0000u) >> 8) | 
-            ((x & 0x0000ff00u) << 8) | ((x & 0x000000ffu) << 24));
+    return (
+        ((x & 0xff000000u) >> 24) | ((x & 0x00ff0000u) >> 8) | ((x & 0x0000ff00u) << 8) | ((x & 0x000000ffu) << 24));
 #endif
 }
 
@@ -49,14 +49,10 @@ static inline uint64_t aws_bswap64_if_be(uint64_t x) {
 #elif defined(__GNUC__) || defined(__clang__)
     return __builtin_bswap64(x);
 #else
-    return  ((x << 56) & 0xff00000000000000ULL) |
-            ((x << 40) & 0x00ff000000000000ULL) |
-            ((x << 24) & 0x0000ff0000000000ULL) |
-            ((x << 8)  & 0x000000ff00000000ULL) |
-            ((x >> 8)  & 0x00000000ff000000ULL) |
-            ((x >> 24) & 0x0000000000ff0000ULL) |
-            ((x >> 40) & 0x000000000000ff00ULL) |
-            ((x >> 56) & 0x00000000000000ffULL);
+    return ((x << 56) & 0xff00000000000000ULL) | ((x << 40) & 0x00ff000000000000ULL) |
+           ((x << 24) & 0x0000ff0000000000ULL) | ((x << 8) & 0x000000ff00000000ULL) |
+           ((x >> 8) & 0x00000000ff000000ULL) | ((x >> 24) & 0x0000000000ff0000ULL) |
+           ((x >> 40) & 0x000000000000ff00ULL) | ((x >> 56) & 0x00000000000000ffULL);
 #endif
 }
 
