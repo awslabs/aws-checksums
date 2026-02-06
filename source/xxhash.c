d26add91d5dd195e9580e0041aab82aff153b208
DmitriyMusatkin <musatkd@amazon.com>
2026-02-06 00:49:13 -0800

    dispatch

--- source/xxhash.c
+++ source/xxhash.c
@@ -9,6 +9,8 @@
 
 #if defined(AWS_USE_CPU_EXTENSIONS) && defined(AWS_ARCH_INTEL_X64)
 #    define XXH_X86DISPATCH
+
+#    define XXH_DISPATCH_SSE2 1
 #    if defined(AWS_HAVE_AVX2_INTRINSICS)
 #       define XXH_DISPATCH_AVX2 1
 #    endif
@@ -376,7 +378,7 @@ int aws_xxhash64_compute(uint64_t seed, struct aws_byte_cursor data, struct aws_
 int aws_xxhash3_64_compute(uint64_t seed, struct aws_byte_cursor data, struct aws_byte_buf *out) {
 #if defined(AWS_ARCH_INTEL_X64)
     AWS_FATAL_ASSERT(s_x86_XXH3_64_seed_compute);
-    XXH64_hash_t hash = s_x86_XXH3_64_seed_compute(data.ptr, data.len, seed);
+    XXH64_hash_t hash = XXH3_64bits_internal(input, len, seed, XXH3_kSecret, sizeof(XXH3_kSecret), s_x86_XXH3_64_seed_compute);
 #else
     XXH64_hash_t hash = XXH3_64bits_withSeed(data.ptr, data.len, seed);
 #endif
@@ -390,7 +392,7 @@ int aws_xxhash3_64_compute(uint64_t seed, struct aws_byte_cursor data, struct aw
 int aws_xxhash3_128_compute(uint64_t seed, struct aws_byte_cursor data, struct aws_byte_buf *out) {
 #if defined(AWS_ARCH_INTEL_X64)
     AWS_FATAL_ASSERT(s_x86_XXH3_128_seed_compute);
-    XXH128_hash_t hash = s_x86_XXH3_128_seed_compute(data.ptr, data.len, seed);
+    XXH128_hash_t hash = XXH3_128bits_internal(input, len, seed, XXH3_kSecret, sizeof(XXH3_kSecret), s_x86_XXH3_128_seed_compute);
 #else
     XXH128_hash_t hash = XXH3_128bits_withSeed(data.ptr, data.len, seed);
 #endif
